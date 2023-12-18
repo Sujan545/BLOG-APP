50e29189f9b4b98b383daba9bd5f49559fc38ffb
Sujan545 <ksuzan545@gmail.com>
2023-12-18 21:18:18 +0545

    dtoadd

---
+++ backend/dto/userDTO.js
@@ -0,0 +1,22 @@
+class UserDTO {
+    constructor(name, username, email, password) {
+      if (!name || !username || !email || !password) {
+        throw new Error("All fields are required");
+      }
+  
+      if (!this.isValidEmail(email)) {
+        throw new Error("Invalid Email format");
+      }
+  
+      this.name = name;
+      this.username = username;
+      this.email = email;
+      this.password = password;
+    }
+  
+    isValidEmail(email) {
+      return /\S+@\S+\.\S+/.test(email);
+    }
+  }
+  
+  module.exports = UserDTO;
